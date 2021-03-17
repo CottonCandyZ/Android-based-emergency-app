2e169f8cd912b81b44a83e1a3c0657230c3f9d52
cotton_candyZ <zztgqk@gmail.com>
2021-03-17 16:47:09 +0800

    add: login fragment and change theme colors

--- app/src/main/java/com/example/emergency/MainActivity.kt
+++ app/src/main/java/com/example/emergency/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.emergency
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
