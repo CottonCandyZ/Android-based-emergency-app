baac4412cebf00acfba50e5b0519917e69f023af
cotton_candyZ <zztgqk@gmail.com>
2021-03-17 18:07:19 +0800

    improve: use dataBinding in login fragment

--- app/src/main/java/com/example/emergency/ui/LoginFragment.kt
+++ app/src/main/java/com/example/emergency/ui/LoginFragment.kt
@@ -1,13 +1,13 @@
 package com.example.emergency.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.emergency.R
+import com.example.emergency.databinding.FragmentLoginBinding
 
 
 /**
@@ -15,18 +15,21 @@ import com.example.emergency.R
  */
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         // test
-        view?.findViewById<Button>(R.id.buttonSignUp)?.setOnClickListener {
+        binding.buttonSignUp.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
         }
     }
