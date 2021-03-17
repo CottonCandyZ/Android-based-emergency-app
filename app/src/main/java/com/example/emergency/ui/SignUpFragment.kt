974c3421b51ca74767c7a1f8fbc4e70c2fdb6318
cotton_candyZ <zztgqk@gmail.com>
2021-03-17 18:04:52 +0800

    add: enter password and improve sign up fragment

--- app/src/main/java/com/example/emergency/ui/SignUpFragment.kt
+++ app/src/main/java/com/example/emergency/ui/SignUpFragment.kt
@@ -1,33 +1,48 @@
 package com.example.emergency.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.emergency.R
-import com.google.android.material.textfield.TextInputLayout
+import com.example.emergency.databinding.FragmentSignUpBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class SignUpFragment : Fragment() {
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        var times = 0
         // test
         view?.findViewById<Button>(R.id.buttonNextStep)?.setOnClickListener {
-            view?.findViewById<TextInputLayout>(R.id.signUpCode)?.visibility = View.VISIBLE
-            view?.findViewById<Button>(R.id.buttonGetCode)?.visibility = View.VISIBLE
+            when (times) {
+                0 -> {
+                    binding.signUpCode.visibility = View.VISIBLE
+                    binding.buttonGetCode.visibility = View.VISIBLE
+                }
+                1 -> {
+                    binding.signUpCode.visibility = View.GONE
+                    binding.buttonGetCode.visibility = View.GONE
+                    binding.signUpPhone.visibility = View.GONE
+                    binding.signUpPasswordFirst.visibility = View.VISIBLE
+                    binding.signUpPasswordSecond.visibility = View.VISIBLE
+                }
+            }
+            times++
         }
     }
 }
